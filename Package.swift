4d97cffd927cace7566dfe0a6f746363648e7634
Jaehyeon Kim <kim.jaehyeon@nbt.com>
2024-04-23 18:26:22 +0900

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.7
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SnowOfwModule",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SnowOfwModule",
+            targets: ["SnowOfwModule"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adison-ads/adison-offerwall-ios-sdk", exact: "3.6.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .binaryTarget(
+            name: "SnowOfwModule",
+            url: "https://github.com/adison-ads/adison-ofw-module-snow-ios/releases/download/0.10.0/SnowOfwModule.zip",
+            checksum: ""
+        ),
+        .target(
+            name: "SnowOfwModuleTarget",
+            dependencies: [.product(name: "AdisonOfferwallSDK", package: "adison-offerwall-ios-sdk")],
+            path: "Sources"
+        )
+    ]
+)
